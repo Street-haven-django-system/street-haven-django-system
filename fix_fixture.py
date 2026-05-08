3b28f6d9f579f40b1ea51373ca0c88e2cd70461c
nataliejadesarinas <nataliejadesarinas@gmail.com>
2026-05-08 11:20:05 +0800

    Add fixtures and deployment fixes

---
+++ fix_fixture.py
@@ -0,0 +1,8 @@
+﻿import json
+with open('fixtures.json', 'rb') as f:
+    content = f.read()
+content = content.decode('latin-1')
+data = json.loads(content)
+with open('fixtures_clean.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False)
+print('Done')
