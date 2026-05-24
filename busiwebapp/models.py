1bab13ffb31826373faf8af0a06be6b93b2e3684
nataliejadesarinas <nataliejadesarinas@gmail.com>
2026-05-24 18:28:18 +0800

    modified models

--- busiwebapp/models.py
+++ busiwebapp/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='profiles/ ', blank=True, null=True)
     contact = models.CharField(max_length=20, blank=True)
     dob = models.DateField(blank=True, null=True)
     address = models.TextField(blank=True)
